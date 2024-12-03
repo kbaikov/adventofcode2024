d52301a01bdda103d63989bf287fd1781b2f5cc4
kbaikov <konstantin.baikov@gmail.com>
2024-12-03 20:21:44 +0100

    Solve day03 part 2

--- day03.py
+++ day03.py
@@ -1,22 +1,11 @@
-"""based on https://github.com/nedbat/adventofcode2023/blob/main/new.py"""
-
-import re
 import pathlib
-from dataclasses import dataclass
+import re
 
 TEST_INPUT = """\
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 """
 
 FILE = pathlib.Path("day03_input.txt").read_text()
-
-
-# def parse_table(text: str) -> list[tuple[str, int]]:
-#     entries: list[tuple[str, int]] = []
-#     for line in text.splitlines():
-#         card, bid = line.split()
-#         entries.append((card, int(bid)))
-#     return entries
 
 
 def part1(text: str) -> int:
@@ -31,18 +20,34 @@ def test_part1():
 
 if __name__ == "__main__":
     answer = part1(FILE)
-    print(answer)
+    # print(answer)
 
 
-# def part2(text: str)-> int:
-#     ...
-#
-#
-# def test_part2():
-#     assert part2(TEST_INPUT) == 123456
-#
-#
-#
-# if __name__ == "__main__":
-#     answer = part2(FILE)
-#     print(answer)
+def part2(text: str) -> int:
+    # text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    # text.replace("\\n", "")
+    # do_matches = [i.end() for i in re.finditer(r"do\(\)", text, re.MULTILINE)]
+    # dont_matches = [i.end() for i in re.finditer(r"don't\(\)", text, re.MULTILINE)]
+    # split idea is from https://www.reddit.com/r/adventofcode/comments/1h5obsr/comment/m08ng5n/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    do_split = text.split("do()")
+    dont_split = [x.split("don't") for x in do_split]
+    result = "".join(x[0] for x in dont_split)
+    # do_matches.insert(0, 0)
+    # dont_matches.insert(len(dont_matches), 99999999999999999999999)
+    # text_ranges = [(a, b) for a, b in zip(do_matches, dont_matches)]
+    # result = ""
+    # for x, y in text_ranges:
+    #     result += text[x:y]
+    # print(do_split)
+    # print(dont_split)
+    # print(result)
+
+    return part1(result)
+
+
+def test_part2():
+    assert part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") == 48
+
+
+if __name__ == "__main__":
+    print(part2(FILE))
